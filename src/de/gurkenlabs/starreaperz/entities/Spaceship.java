373c34f5ddc70052e982f2d0de9ba2f0e12421e3
Steffen <mail@steffen-wilke.com>
2023-01-07 21:19:16 +0100

    Removed sprites from git

--- src/de/gurkenlabs/starreaperz/entities/Spaceship.java
+++ src/de/gurkenlabs/starreaperz/entities/Spaceship.java
@@ -1,16 +1,17 @@
 package de.gurkenlabs.starreaperz.entities;
 
+import de.gurkenlabs.litiengine.attributes.RangeAttribute;
+import de.gurkenlabs.litiengine.entities.CombatInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
 import de.gurkenlabs.litiengine.entities.MovementInfo;
 import de.gurkenlabs.litiengine.graphics.animation.IEntityAnimationController;
 import de.gurkenlabs.litiengine.physics.IMovementController;
 
 @MovementInfo(acceleration = 1500, deceleration = 3000, velocity = 200)
+@CombatInfo(hitpoints = 3)
 public class Spaceship extends Creature {
   public Spaceship() {
     super("spaceship");
-
-    System.out.println("Spaceship loaded");
   }
 
   @Override
