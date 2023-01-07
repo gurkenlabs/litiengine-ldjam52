8bc359503495431a0c4a4a5664ae92fbb654bc2d
nightm4re94 <matthias@gurkenlabs.de>
2023-01-07 11:57:04 +0100

    Init project structure

---
+++ src/de/gurkenlabs/starreaperz/Program.java
@@ -0,0 +1,31 @@
+package de.gurkenlabs.starreaperz;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
+
+public class Program {
+
+  public static void main(String[] args) {
+    // set meta information about the game
+    Game.info().setName("Star ReaperZ");
+    Game.info().setSubTitle("Fick die Sterne, Alter!");
+    Game.info().setVersion("v0.69");
+    Game.info().setWebsite("https://github.com/gurkenlabs/litiengine-ldjam52");
+    Game.info().setDescription("Woke Sternenschnitter ernten die ganze Energie ey, was geeeeht?");
+
+    // init the game infrastructure
+    Game.init(args);
+
+    // set the icon for the game (this has to be done after initialization
+    // because the ScreenManager will not be present otherwise)
+    Game.window().setIcon(Resources.images().get("icon.png"));
+    Game.graphics().setBaseRenderScale(3f);
+
+    // load data from the utiLITI game file
+    Resources.load("game.litidata");
+    //        Game.screens().add(new MenuScreen());
+    //        Game.screens().add(new IngameScreen());
+
+    Game.start();
+  }
+}
